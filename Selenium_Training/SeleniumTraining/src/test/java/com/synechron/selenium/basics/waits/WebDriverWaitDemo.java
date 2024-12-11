51935fa887e30cf219feae678270c047e9166c9d
Aravinda HB <aru03.info@gmail.com>
2024-12-11 11:23:22 +0530

    selenium

--- Selenium_Training/SeleniumTraining/src/test/java/com/synechron/selenium/basics/waits/WebDriverWaitDemo.java
+++ Selenium_Training/SeleniumTraining/src/test/java/com/synechron/selenium/basics/waits/WebDriverWaitDemo.java
@@ -7,15 +7,26 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.FluentWait;
 import org.openqa.selenium.support.ui.WebDriverWait;
-import org.testng.annotations.Test;
+import org.testng.annotations.*;
 
 import java.time.Duration;
 
 public class WebDriverWaitDemo extends ApplicationUtils {
 
+    @BeforeMethod
+    public void prepare() {
+        getDriver("edge");
+    }
+
+    @AfterMethod
+    public void cleanUp() {
+        close();
+    }
+
+
     @Test
     public void webDriverWaitDemo() {
-        getDriver("edge");
+
         launchApplication("file:///C:/Trainings/Synechron_02_12/Selenium_Training/html/tiimeout.html");
         click("tagname", "button");
         //Step-1 Create a wait object by passing max timeout
@@ -29,7 +40,6 @@ public class WebDriverWaitDemo extends ApplicationUtils {
 
     @Test
     public void fluentWaitDemo() {
-        getDriver("edge");
         launchApplication("file:///C:/Trainings/Synechron_02_12/Selenium_Training/html/tiimeout.html");
         click("tagname", "button");
 
@@ -64,9 +74,31 @@ public class WebDriverWaitDemo extends ApplicationUtils {
 
     @Test
     public void fluentwaitDemo2() {
-        getDriver("edge");
         launchApplication("file:///C:/Trainings/Synechron_02_12/Selenium_Training/html/CounterText.html");
 
+        FluentWait<WebElement> wait = new FluentWait<WebElement>
+                (getWebElement("id", "counter")).
+                withTimeout(Duration.ofSeconds(60)).
+                pollingEvery(Duration.ofMillis(100)).
+                ignoring(NoSuchElementException.class).
+                ignoring(Exception.class);
+
+
+        //Step-2 Create an object to function interface and keep your own wait logic
+        Function<WebElement, Boolean> fun = new Function<WebElement, Boolean>() {
+            public Boolean apply(WebElement element) {
+                if (element.getText().equals("25")) {
+                    System.out.println("Element is displayed");
+                    return true;
+                } else {
+                    System.out.println("Element is not displayed");
+                    return false;
+                }
+            }
+        };
+
+        //step-3 use until method and pass fun object
+        wait.until(fun);
 
 
 
