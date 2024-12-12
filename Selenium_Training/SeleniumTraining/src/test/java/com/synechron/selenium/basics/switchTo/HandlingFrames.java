70aff79c11a8f0fb07b63166a10f10473f9927b7
Aravinda HB <aru03.info@gmail.com>
2024-12-12 11:48:09 +0530

    url

---
+++ Selenium_Training/SeleniumTraining/src/test/java/com/synechron/selenium/basics/switchTo/HandlingFrames.java
@@ -0,0 +1,38 @@
+package com.synechron.selenium.basics.switchTo;
+
+import com.synechron.selenium.basics.utils.ApplicationUtils;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
+import org.testng.annotations.Test;
+
+public class HandlingFrames extends ApplicationUtils
+{
+
+
+    @Test
+    public void dragAndDropDemo() throws InterruptedException {
+
+        getDriver("edge");
+        launchApplication("https://jqueryui.com/droppable/");
+        driver.switchTo().frame(getWebElement("xpath","//iframe[@class='demo-frame']"));
+        WebElement sourceElement = getWebElement("id", "draggable");
+        WebElement destElement = getWebElement("id", "droppable");
+
+        System.out.println("Before drag and drop");
+        System.out.println("Source TExt " + sourceElement.getText());
+        System.out.println("Dest TExt " + destElement.getText());
+
+        Actions actions = new Actions(driver);
+        actions.dragAndDrop(sourceElement, destElement).perform();
+
+        System.out.println("Before drag and drop AFTER ");
+        System.out.println("Source TExt " + sourceElement.getText());
+        System.out.println("Dest TExt " + destElement.getText());
+
+        driver.switchTo().defaultContent();
+        click("linktext","Draggable");
+        Thread.sleep(2000);
+
+    }
+
+}
--- Selenium_Training/SeleniumTraining/src/test/java/com/synechron/selenium/basics/switchTo/HandlingMultiBrowser.java
+++ Selenium_Training/SeleniumTraining/src/test/java/com/synechron/selenium/basics/switchTo/HandlingMultiBrowser.java
@@ -44,4 +44,6 @@ public class HandlingMultiBrowser extends ApplicationUtils
 
     }
 
+
+
 }
