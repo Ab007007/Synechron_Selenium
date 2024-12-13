ffafb0197e6eddc24ce0153c0144e5990fd7c404
Aravinda HB <aru03.info@gmail.com>
2024-12-13 12:42:41 +0530

    testng

---
+++ Selenium_Training/SeleniumTraining/src/test/java/com/synechron/selenium/basics/testng/HelloWorldTestNG.java
@@ -0,0 +1,11 @@
+package com.synechron.selenium.basics.testng;
+
+import org.testng.annotations.Test;
+
+public class HelloWorldTestNG {
+
+    @Test
+    public void testHelloWorld() {
+        System.out.println("Hello World");
+    }
+}
---
+++ Selenium_Training/SeleniumTraining/src/test/java/com/synechron/selenium/basics/testng/TestNGAllAnnotations.java
@@ -0,0 +1,73 @@
+package com.synechron.selenium.basics.testng;
+
+import org.testng.annotations.*;
+
+public class TestNGAllAnnotations {
+
+
+
+    @DataProvider
+    public Object[][] dpMethod() {
+
+        return new Object[][]{
+                
+        };
+    }
+
+    @BeforeSuite
+    public void beforeSuite() {
+        System.out.println("Before Suite");
+    }
+    @AfterSuite
+    public void afterSuite() {
+        System.out.println("After Suite");
+    }
+    @BeforeTest
+    public void beforeTest() {
+        System.out.println("Before Test");
+
+    }
+
+    @AfterTest
+    public void afterTest()
+    {
+        System.out.println("After Test");
+
+    }
+
+    @BeforeClass
+    public void beforeClass() {
+        System.out.println("Before Class");
+
+    }
+
+    @AfterClass
+    public void afterClass() {
+        System.out.println("After Class");
+
+
+    }
+
+    @BeforeMethod
+    public void beforeMethod() {
+         System.out.println("Before Method");
+
+    }
+
+    @AfterMethod
+    public void afterMethod() {
+        System.out.println("After Method");
+    }
+
+    @Test
+    public void test1() {
+        System.out.println("Test 1");
+    }
+
+    @Test
+    public void test2() {
+        System.out.println("Test 2");
+    }
+
+
+}
